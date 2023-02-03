880aa6c8585f1347c5d6bf114c64b9f3bc166a04
Yuri Miguel <113549805+YuriMSdS@users.noreply.github.com>
2023-02-03 19:05:37 -0300

    Atualizar o main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  username             = "yuri"
-  password             = "Ym319733"
+  username             = "seu_usuario"
+  password             = "sua_senha"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
